788c22d55487bb71a751581c0aa28ec539ccd9e7
treyhest <70986323+treyhest@users.noreply.github.com>
2021-09-24 00:36:17 -0500

    Unfinished scripts starting XML parsing as well as a few Example XMLs
    Not intended to merge, only for public viewing and discussion with team.

---
+++ Levels Of Mystery/Assets/DialogueTree/DialogueNode.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace DialogueTree
+{
+    //unused class  meant to hold all the relevant data of a Node and or it's links, unnecesary with XmlNodeList, may still be useful here for Direct Graphing
+    public class DialogueNode {
+
+        public DialogueNode(int id, string text, int[] nextNodes){
+        }
+    }
+}
