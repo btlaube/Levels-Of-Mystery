2a68f00ca875684796849f5d99f43d67ab9954d7
treyhest <70986323+treyhest@users.noreply.github.com>
2021-10-13 21:10:43 -0500

    Dialogue navigation is near completely functional.

--- Levels Of Mystery/Assets/DialogueTree/DialogDisplay.cs
+++ Levels Of Mystery/Assets/DialogueTree/DialogDisplay.cs
@@ -2,30 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Xml;
+using System.Linq;
 
 
 public class DialogDisplay : MonoBehaviour
 {
-    public List<string> Choices;
+    public Dictionary<string, string> Choices = new Dictionary<string, string>();
     public int ChoiceCount;
-    public string response;
     public Text textElement;
     public Dialogue Tree = new Dialogue();
 
     void Start(){
         Tree.LoadDialogue();
-        Choices.Add(Tree.CurrentNode.InnerText);
+        Choices.Add(Tree.CurrentNode.Attributes["ID"].Value, Tree.CurrentNode.InnerText);
+        Debug.Log($"{Choices.Values}, {Choices.Keys}");
+        Debug.Log(Choices.Count);
         DisplayChoices();
+        
     }
 
-    public void ChangeChoices(int choice = 0){
-        //uses optional parameter choice to update the list of choices. Calls DisplayChoices to update screen.
-        if (choice == 0) {
-            Choices = Tree.GetNext();
-        } else {
-            Choices = Tree.GetNext();
-        }
+    public void ChangeChoices(string choice){
+        Choices = Tree.GetNext(choice);
         DisplayChoices();
     }
 
@@ -33,36 +30,39 @@ public class DialogDisplay : MonoBehaviour
         //formats Choices list to fit text box. (Theres probably a beter way to do this with unity, but this was what I came up with to display arbitrary amount of options)
         ChoiceCount = Choices.Count;
         textElement.text = "";
-        if (ChoiceCount > 1){
+        if (ChoiceCount > 1){ //remember to change
             int index = 1;
-            foreach (string choice in Choices){
+            foreach (string choice in Choices.Values){
                 Debug.Log(choice);
                 textElement.text += $"{index} - {choice} \n";
                 index+=1;
             }
         } else {
-            textElement.text += $"{Choices[0]}";
+            textElement.text += $"{Choices.ElementAt(0).Value}";
         }
     }
 
     void Update(){
         //listens for enter/number key/ to update text box and perform necessary operations.
-        int ChoiceCount = Choices.Count; //it is inneficient to count choices text each frame update.
-        //Debug.Log(Choices.Count);
+        ChoiceCount = Choices.Count; //it  may inneficient to count choices  each frame update.
         if (ChoiceCount > 1){
             for(int i=1;i<=ChoiceCount;i++) {
                 if(Input.GetKeyUp((KeyCode)(48+i))){
-                    Debug.Log("you pressed a key");
-                    ChangeChoices(i);
+                    Debug.Log($"you pressed {i}");
+                    string id = Choices.ElementAt(i-1).Key;
+                    Debug.Log($"chosen id is : {id}");
+                    ChangeChoices(id);
                 }
             }
         } else if (ChoiceCount == 1){
             if (Input.GetKeyUp("return")){
-                Debug.Log("you pressed a key");
-                ChangeChoices();
+                Debug.Log("you pressed return");
+                string id = Choices.ElementAt(0).Key;
+                Debug.Log($"chosen id is : {id}");
+                ChangeChoices(id);
             }
         } else {
-            // textElement.text = "end";
+            textElement.text = "end";
         }   
     }
 }
--- Levels Of Mystery/Assets/DialogueTree/Dialogue.cs
+++ Levels Of Mystery/Assets/DialogueTree/Dialogue.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Xml;
-using System.Xml.Serialization;
-using System.Xml.Linq;
+
 using System.IO;
 
 
@@ -11,33 +10,37 @@ public class Dialogue
 {
     public XmlDocument Doc = new XmlDocument();
     public XmlNode CurrentNode;
-    public List<string> NextChoices;
 
     public void LoadDialogue(string path="Assets\\Resources\\Example.xml"){ //may need to be text stream
         if (File.Exists(path)){ 
             //XmlDocument Doc = new XmlDocument();
             Doc.Load(path);
             CurrentNode = Doc.FirstChild.FirstChild; //first element after the root.
-            Debug.Log(CurrentNode.InnerText);
+            //Debug.Log(CurrentNode.InnerText);
             //CurrentNode =  Doc.SelectSingleNode($"/Root/Node[@ID='{102}']");
         }
     }
 
-    public List<string> GetNext(){
-        List<string> options = new List<string>();
+    public void changeCurrentNode(string address){
+        CurrentNode = Doc.SelectSingleNode($"Root/Node[@ID='{address}']");
+    }
+
+    public Dictionary<string, string> GetNext(string id){
+        CurrentNode = Doc.SelectSingleNode($"Root/Node[@ID='{id}']");
+        Dictionary<string, string> options = new Dictionary<string, string>();
         foreach (string address in CurrentNode["Address"].InnerText.Split()){
-            Debug.Log(address);
-            Debug.Log(Doc);
+            //Debug.Log(address);
+            //Debug.Log(Doc);
             XmlNode next = Doc.SelectSingleNode($"Root/Node[@ID='{address}']");
-            Debug.Log($"Root/Node[@ID='{address}']");
+            //Debug.Log($"Root/Node[@ID='{address}']");
             //add conditional here for if node is valid (reputation etc.)
-            Debug.Log(next.InnerText);
-            options.Add(next.InnerText);
+            //Debug.Log(next.InnerText);
+            options.Add(address, next.InnerText);
         }
         return options;
     }
 
-    public void changeCurrentNode(string id){
-        CurrentNode = Doc.SelectSingleNode($"Node[@ID='{id}']");
-    }
+    // public void changeCurrentNode(string id){
+    //     CurrentNode = Doc.SelectSingleNode($"Node[@ID='{id}']");
+    // }
 }
