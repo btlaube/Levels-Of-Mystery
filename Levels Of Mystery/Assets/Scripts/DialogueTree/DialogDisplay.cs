84c23af081d79666f7df6fdd73791ce6aff52ed3
treyhest <70986323+treyhest@users.noreply.github.com>
2021-11-14 19:04:00 -0600

    Added Starter code for detecting what character is speaking.

--- Levels Of Mystery/Assets/Scripts/DialogueTree/DialogDisplay.cs
+++ Levels Of Mystery/Assets/Scripts/DialogueTree/DialogDisplay.cs
@@ -12,16 +12,18 @@ public class DialogDisplay : MonoBehaviour
     public Text textElement;
     public Dialogue Tree = new Dialogue();
     public string Path = "";
-  
+
     public Player player;
 
-    void Start(){
+    void Start()
+    {
         //restart scene on new tree.
         Debug.Log(player.time);
         getPath();
     }
 
-    public void getPath() {
+    public void getPath()
+    {
         if (player.day == 10) //arbitrary
         {
             Tree = null; //will cause error, only for testing
@@ -32,42 +34,57 @@ public class DialogDisplay : MonoBehaviour
         ChangeChoices(Tree.CurrentNode.Attributes["ID"].Value);
     }
 
-    public void ChangeChoices(string choice){
+    public void ChangeChoices(string choice)
+    {
         Choices = Tree.GetNext(choice);
         DisplayChoices();
     }
 
-    public void DisplayChoices(){
+    public void DisplayChoices()
+    {
         //formats Choices list to fit text box. (Theres probably a beter way to do this with unity, but this was what I came up with to display arbitrary amount of options)
         ChoiceCount = Choices.Count;
         textElement.text = "";
-        if (ChoiceCount > 1){
+        if (ChoiceCount > 1)
+        {
             int index = 1;
-            foreach (string choice in Choices.Values){
+            foreach (string choice in Choices.Values)
+            {
                 Debug.Log(choice);
                 textElement.text += $"{index} - {choice} \n";
-                index+=1;
+                index += 1;
             }
-        } else if (ChoiceCount == 1) {
+        }
+        else if (ChoiceCount == 1)
+        {
             textElement.text += $"{Choices.ElementAt(0).Value}";
         }
     }
 
-    void Update(){
+    void Update()
+    {
         //listens for enter/number key/ to update text box and perform necessary operations.
-        if (ChoiceCount > 1){
-            for(int i=1;i<=ChoiceCount;i++) {
-                if(Input.GetKeyUp((KeyCode)(48+i))){
-                    string id = Choices.ElementAt(i-1).Key;
+        if (ChoiceCount > 1)
+        {
+            for (int i = 1; i <= ChoiceCount; i++)
+            {
+                if (Input.GetKeyUp((KeyCode)(48 + i)))
+                {
+                    string id = Choices.ElementAt(i - 1).Key;
                     ChangeChoices(id);
                 }
             }
-        } else if (ChoiceCount == 1){
-            if (Input.GetKeyUp("return")){
+        }
+        else if (ChoiceCount == 1)
+        {
+            if (Input.GetKeyUp("return"))
+            {
                 string id = Choices.ElementAt(0).Key;
                 ChangeChoices(id);
             }
-        } else {
+        }
+        else
+        {
             textElement.text = "end";
             if (Tree != null) //checks if we have progressed through at least one tree.
             {
