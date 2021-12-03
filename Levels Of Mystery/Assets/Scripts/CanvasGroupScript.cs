9323a98cbfce8e95cf12db8c7b22c0075d8eab3e
Ben Laube <blaube2@huskers.unl.edu>
2021-12-03 00:06:03 -0600

    slight changes to canvasGroup ordering

--- Levels Of Mystery/Assets/Scripts/CanvasGroupScript.cs
+++ Levels Of Mystery/Assets/Scripts/CanvasGroupScript.cs
@@ -23,21 +23,6 @@ public class CanvasGroupScript : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    //public void Update() {
-    //    currentScene = SceneManager.GetActiveScene().name;
-    //    if (currentScene == "Game") {
-    //        transform.GetChild(0).gameObject.SetActive(false);
-    //        transform.GetChild(1).gameObject.SetActive(true);
-    //        transform.GetChild(3).gameObject.SetActive(true);
-    //    }
-    //    else if (currentScene == "MainMenu") {
-    //        transform.GetChild(0).gameObject.SetActive(true);
-    //        transform.GetChild(1).gameObject.SetActive(false);
-    //        transform.GetChild(3).gameObject.SetActive(false);
-    //        transform.GetChild(4).gameObject.SetActive(false);
-    //    }
-    //}
-
     public void LoadGameScene() {
         transform.GetChild(0).gameObject.SetActive(false);
         transform.GetChild(1).gameObject.SetActive(true);
